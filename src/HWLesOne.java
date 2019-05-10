01921828c6fbb796de2aaa199ce15afd5482bbc2
evgen <lunyov.evgeniy@gmail.com>
2019-05-11 00:59:49 +0300

    HW-001

--- src/HWLesOne.java
+++ src/HWLesOne.java
@@ -1,6 +1,32 @@
 public class HWLesOne {
 
     public static void main(String[] args) {
-        
+        // subTask - 1
+        String myName = "Evgen";
+        int myAge = 36;
+        float myWeight = 93.5f;
+
+        System.out.println("subTask - 1 \n" + "Имя = " + myName + ", " + "Возраст = " + myAge + ", " + "Вес = " + myWeight);
+
+        // subTask - 2
+        int valyeA = 1;
+        int valyeB = 6;
+
+        System.out.println("subTask - 2.1 \n" + "a = " + valyeA + "\n" + "b = " + valyeB);
+        int valyeC = valyeA;
+        valyeA = valyeB;
+        valyeB = valyeC;
+
+        System.out.println("subTask - 2.2 \n" + "a = " + valyeA + "\n" + "b = " + valyeB);
+
+        // subTask - 3
+        valyeA = valyeA + valyeB;
+        valyeB = valyeB - valyeA;
+        valyeB = -valyeB;
+        valyeA = valyeA - valyeB;
+
+        System.out.println("subTask - 3 \n" + "a = " + valyeA + "\n" + "b = " + valyeB);
+
+
     }
 }
