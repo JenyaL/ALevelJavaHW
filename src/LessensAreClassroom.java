37f042a6956bfcddfdab4b18fabc06a836ba8eb5
evgen <lunyov.evgeniy@gmail.com>
2019-05-22 00:13:38 +0300

    Home

---
+++ src/LessensAreClassroom.java
@@ -0,0 +1,29 @@
+public class LessensAreClassroom {
+    public static void main(String[] args) {
+
+        String str = "QWERTY YTREWQ";
+
+        str.length(); // длина
+        System.out.println(str.length() + " длина");
+
+        str.charAt(1); // символ под номером
+        System.out.println(str.charAt(0) + " символ под номером");
+
+        str.concat("String char"); // склейка
+        System.out.println(str.concat("- + NEW" + " склейка"));
+
+        str.substring(1); // игнорирует до нужного символа
+        System.out.println(str.substring(5) + " игнорирует до нужного символа");
+
+        str.equalsIgnoreCase("R"); // игнорирует значение (false or true)
+        System.out.println(str.equalsIgnoreCase("QWERTY_YTREWQ") + " игнорирует значение");
+
+        str.endsWith("R"); // проверяет на что заканчивается (false or true)
+        System.out.println(str.endsWith("Q") + " проверяет на что заканчивается");
+
+        str.split(" ", 1); // ищет слова в строке для передачи в МАССИВ !!!
+
+        str.replace("c", ""); // понять!!!
+
+    }
+}
