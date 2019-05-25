c2edbdf97f905987c3e3af51ac0cd02fa1eeca7d
Jenya <lunyov.evgeniy@gmail.com>
2019-05-25 13:35:24 +0300

    Home

---
+++ src/HWLessFour.java
@@ -0,0 +1,60 @@
+public class HWLessFour {
+
+
+    public static void main(String[] args) {
+/*
+        String[] qwerty = new String[6];
+        int i = 0;
+        for (String num : qwerty) {
+            qwerty[i] = "q" + i;
+            System.out.print(qwerty[i] + " ");
+            i++;
+        }*/
+
+        int a = 3;
+        int b = 4;
+
+      /* double/* double a = 5.0;
+        double b = 2.0;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);*/
+
+        /*int w = 8;
+        int e = 8;
+        int r = 8;
+        int p = 8;
+        System.out.println(++w);
+        System.out.println(e++);
+        System.out.println(--r);
+        System.out.println(p--);*/
+
+       /* System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a <b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);*/
+
+
+int q = 0;
+        /*for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }*/
+        while (q <= 100) {
+            if (q % 2 == 0) {
+                System.out.println(q);
+            }
+            q++;
+        }
+    }
+
+
+
+
+}
