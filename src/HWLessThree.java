c57c3cc36f5ca4f65d3f76c2fd1298a0bebd7abb
evgen <lunyov.evgeniy@gmail.com>
2019-05-25 21:08:07 +0300

    HomeTaskThree

--- src/HWLessThree.java
+++ src/HWLessThree.java
@@ -1,34 +1,12 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class HWLessThree {
     public static void main(String[] args) {
-        /*Scanner scanObj = new Scanner(System.in);
-        System.out.println("Enter symbvol");
-        int a = scanObj.nextInt();
-        System.out.println("enter next symbvol");
-        int b = scanObj.nextInt();
-        int result = a + b;
-        System.out.println(result);*/
-
-        /*
-        Scanner scanObj = new Scanner(System.in);
-        String b = scanObj.next();
-        switch (b) {
-
-            case "a":
-                System.out.println("nocorrect");
-                break;
-            case "c":
-                System.out.println("corect");
-                break;
-        }*/
-        /*4. Создайте массив из имен городов, например:
-«Москва», «Амстердам», «Минск», «Курск», «Киев».
+        /*4. Создайте массив из имен городов, например: «Москва», «Амстердам», «Минск», «Курск», «Киев».
         Напишите игру в города в компьютером ☺
         Процесс такой:
         Как только игра начинается в консоль выводится надпись:
-«Давай поиграем в ‘Города’. Ты первый называй!»
+        «Давай поиграем в ‘Города’. Ты первый называй!»
         Пользователь печатает в консоль, имя города, компьютер смотрит на
         последнюю букву, и ищет в массиве значения которые начинаются с
         указанной буквы. Компьютеру нельзя использовать один и тот же город
@@ -36,43 +14,31 @@ public class HWLessThree {
         весь свой запас вывести в консоль: «Я проиграл, ты знаешь больше
         городов».*/
         String citys = "Москва, Амстердам, Минск, Курск, Киев";
-
-        // преоброзовать строку в массив
         String[] arrayCity = citys.split(", ", 0);
-
-        // создание массив адля хранения использованных слов
-        int sizeArrayUseWord = 1;
-        String[] arrayUseWord = new String[sizeArrayUseWord];
-
-        // начало Игры
         System.out.println("Давай поиграем в ‘Города’. Ты первый называй!");
-
-
-        for (int i = 0; i < arrayCity.length; i++) {
-            // ввод в консоль слова
+        int end = 0;
+        for (int q = 0; q <= arrayCity.length; q++) {
             Scanner scanEnteredConsole = new Scanner(System.in);
             String consoleIn = scanEnteredConsole.next();
-
-            // полцчаем последнюю букву слова из консоли
-            char charWordFromConsoleIn = consoleIn.charAt(consoleIn.length() - 1);
-            String strWordFromConsoleIn = String.valueOf(charWordFromConsoleIn);
-
-            // добавление слова в архивный массив
-            arrayUseWord[i] = strWordFromConsoleIn;
-
-            // ищем первую букву всех слов в масииве
-            String strWordFromArrayCity = arrayCity[i];
-            strWordFromArrayCity.charAt(0);
-
-            // ищем слово которое начинается на последнюю букву слова из консоли
-            if (strWordFromConsoleIn.equalsIgnoreCase(String.valueOf(strWordFromArrayCity.charAt(0)))) {
-
-                System.out.println(strWordFromConsoleIn + " " + strWordFromArrayCity.charAt(0));
-
-
+            for (int i = 0; i <= arrayCity.length; i++) {
+                char charWordFromConsoleIn = consoleIn.charAt(consoleIn.length() - 1);
+                String strWordFromConsoleIn = String.valueOf(charWordFromConsoleIn);
+                String strWordFromArrayCity = arrayCity[i];
+                strWordFromArrayCity.charAt(0);
+                if (strWordFromConsoleIn.equalsIgnoreCase(String.valueOf(strWordFromArrayCity.charAt(0)))) {
+                    System.out.println(strWordFromArrayCity);
+                    System.out.println("тебе на - " + strWordFromArrayCity.charAt(strWordFromArrayCity.length() - 1));
+                    arrayCity[i] = "null";
+                    break;
+                } else if (i == arrayCity.length - 1) {
+                    end = arrayCity.length - 1;
+                    break;
+                }
             }
-
-
+            if (end == arrayCity.length - 1) {
+                System.out.println("Я проиграл, ты знаешь больше городов");
+                break;
+            }
         }
     }
 }
