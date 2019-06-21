8f47a384fb6217317824b1876243c7df46d57920
Jenya <lunyov.evgeniy@gmail.com>
2019-06-21 23:44:18 +0300

    Task

---
+++ src/Task/TaskFirst.java
@@ -0,0 +1,20 @@
+package Task;
+
+
+import java.util.Arrays;
+
+public class TaskFirst {
+
+    public static void main(String[] args) {
+
+        int firstIndex = 0;
+        int nextIndex = 1;
+        String str = "FббmFm";
+
+        char[] ff = str.toCharArray();
+        Arrays.sort(ff);
+        System.out.println(ff);
+
+    }
+}
+
