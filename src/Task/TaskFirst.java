b27aabcde02e19715d984a955b103d3c4f0eb003
Jenya <lunyov.evgeniy@gmail.com>
2019-06-22 00:06:41 +0300

    Status

--- src/Task/TaskFirst.java
+++ src/Task/TaskFirst.java
@@ -1,5 +1,13 @@
 package Task;
 
+/*
+Написать программу, которая считает длину
+(количество символов) самой длинной подстроки без повторяющихся символов.
+"Например аббабвбввбббваа Будет 3"
+ИЛИ ааааа - 1
+ИЛИ пввкев - 3
+
+*/
 
 import java.util.Arrays;
 
@@ -7,13 +15,15 @@ public class TaskFirst {
 
     public static void main(String[] args) {
 
-        int firstIndex = 0;
-        int nextIndex = 1;
-        String str = "FббmFm";
+        String str = "112211";
+        int start = 0;
+        int next = 1;
+
 
         char[] ff = str.toCharArray();
         Arrays.sort(ff);
         System.out.println(ff);
+
 
     }
 }
